ef3b29ba0faae5eac258a551f8ece7056a941414
Sameer Bhati <bhati.sameer987@gmail.com>
2025-06-09 12:51:44 +0530

    Initial Commit

---
+++ LightTests/LightTests.swift
@@ -0,0 +1,17 @@
+//
+//  LightTests.swift
+//  LightTests
+//
+//  Created by Sameer Bhati on 09/06/25.
+//
+
+import Testing
+@testable import Light
+
+struct LightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
